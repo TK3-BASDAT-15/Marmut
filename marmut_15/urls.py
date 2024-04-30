993f954b266692ec49d8620ce37ac067badc85df
adipppp <adhif2004@gmail.com>
2024-04-30 14:04:24 +0700

    Implement Album CRUD

--- marmut_15/urls.py
+++ marmut_15/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("podcast/", include("podcast.urls"))
+    path("podcast/", include("podcast.urls")),
+    path('album/', include('crud_album.urls'))
 ]
