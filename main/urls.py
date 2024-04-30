065d3b29afb920dc6622d36e0b500302207aa746
Oey Joshua Jodrian <joshuajodrian@gmail.com>
2024-04-30 14:31:31 +0700

    tugas putih

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from main.views import show_main, register #sesuaikan dengan nama fungsi yang dibuat
+from main.views import login_user #sesuaikan dengan nama fungsi yang dibuat
+from main.views import logout_user
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('register/', register, name='register'), #sesuaikan dengan nama fungsi yang dibuat
+    path('login/', login_user, name='login'), #sesuaikan dengan nama fungsi yang dibuat
+    path('logout/', logout_user, name='logout'),
+
+
+]
