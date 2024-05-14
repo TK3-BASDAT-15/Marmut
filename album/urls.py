b6330aae891acb420d03fe6c05ab3594fe660ff2
adipppp <adhif2004@gmail.com>
2024-05-14 13:36:56 +0700

    Refactor urls and views of album app

--- album/urls.py
+++ album/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 
 from .views import *
 
+single_album_view = SingleAlbumView.as_view()
+
 urlpatterns = [
     path('', AlbumView.as_view()),
-    path('label/', AlbumView.as_view()),
-    path('add-album/', AlbumView.as_view()),
-    path('<uuid:id_album>/songs/', AlbumView.as_view()),
-    path('<uuid:id_album>/songs/label/', AlbumView.as_view()),
-    path('<uuid:id_album>/add-song/', AlbumView.as_view()),
-    path('<uuid:id_album>/delete/', AlbumView.as_view()),
+    path('<uuid:id_album>/songs/', single_album_view),
+    path('<uuid:id_album>/add-song/', single_album_view),
+    path('<uuid:id_album>/delete/', single_album_view),
 ]
--- album/views.py
+++ album/views.py
@@ -1,16 +1,13 @@
 from django.db import connection
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.shortcuts import render
-from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.decorators.csrf import csrf_exempt
 import json
 import uuid
 
 # Create your views here.
-# @method_decorator(csrf_exempt, name='dispatch')
 class AlbumView(View):
-    def __get_album_list_page(self, request: HttpRequest):
+    def __get_album_list(self, request: HttpRequest):
         with connection.cursor() as cursor:
             query = 'SELECT album.id, judul, nama, jumlah_lagu, total_durasi FROM album, label WHERE album.id_label = label.id'
             cursor.execute(query)
@@ -23,46 +20,50 @@ class AlbumView(View):
 
         return render(request, 'albumList.html', context=context)
     
-    def __get_add_song_page(self, request: HttpRequest, id_album):
+    def __get_add_album(self, request: HttpRequest):
         with connection.cursor() as cursor:
-            query = 'SELECT judul FROM album WHERE album.id = %s'
-            cursor.execute(query, [id_album])
-
-            album_name = cursor.fetchone()[0]
-
-            query = 'SELECT id, nama FROM artist, akun WHERE artist.email_akun = akun.email'
+            query = 'SELECT id, nama FROM label'
             cursor.execute(query)
 
             entries = cursor.fetchall()
-            columns = ['id', 'nama']
-            artists = [dict(zip(columns, row)) for row in entries]
+            columns = [col[0] for col in cursor.description]
+            labels = [dict(zip(columns, row)) for row in entries]
 
-            query = 'SELECT id, nama FROM songwriter, akun WHERE songwriter.email_akun = akun.email'
-            cursor.execute(query)
+        context = {'labels': labels}
 
-            entries = cursor.fetchall()
-            columns = ['id', 'nama']
-            songwriters = [dict(zip(columns, row)) for row in entries]
-
-            query = 'SELECT genre.id_konten, genre.genre FROM genre, song WHERE genre.id_konten = song.id_konten'
-            cursor.execute(query)
-
-            entries = cursor.fetchall()
-            columns = ['id_konten', 'genre']
-            genres = [dict(zip(columns, row)) for row in entries]
-
-        context = {
-            'album_name': album_name,
-            'artists': artists,
-            'songwriters': songwriters,
-            'genres': genres
-        }
-
-        return render(request, 'addSongToAlbum.html', context=context)
+        return render(request, 'addAlbum.html', context=context)
     
-    def __get_album_songs_page(self, request: HttpRequest, id_album):
+    def __post_add_album(self, request: HttpRequest):
+        req_body_dict = json.loads(request.body)
+
+        id_album = uuid.uuid4()
+        judul = req_body_dict['judul']
+        id_label = req_body_dict['id_label']
+        
         with connection.cursor() as cursor:
-            query = 'SELECT konten.judul, konten.durasi, song.id_konten, song.total_play, song.total_download FROM konten, song, album WHERE song.id_album = album.id AND konten.id = song.id_konten AND album.id = %s'
+            query = 'INSERT INTO album (id, judul, id_label) VALUES (%s, %s, %s)'
+            cursor.execute(query, [id_album, judul, id_label])
+
+        return HttpResponse(status=201)
+    
+    def get(self, request: HttpRequest):
+        req_full_path = request.get_full_path()
+
+        if req_full_path.endswith('/add-album/'):
+            return self.__get_add_album(request)
+        elif req_full_path.endswith('/'):
+            return self.__get_album_list(request)
+        
+    def post(self, request: HttpRequest):
+        req_full_path = request.get_full_path()
+
+        if req_full_path.endswith('/add-album/'):
+            return self.__post_add_album(request)
+    
+class SingleAlbumView(View):
+    def __get_songs(self, request: HttpRequest, id_album):
+        with connection.cursor() as cursor:
+            query = 'SELECT konten.judul, konten.durasi, song.id_konten, song.total_play, song.total_download FROM konten JOIN song ON konten.id = song.id_konten JOIN album ON song.id_album = album.id WHERE album.id = %s'
             cursor.execute(query, [id_album])
 
             entries = cursor.fetchall()
@@ -80,60 +81,65 @@ class AlbumView(View):
 
         return render(request, 'albumSongs.html', context=context)
     
-    def __get_add_album_page(self, request: HttpRequest):
+    def __get_add_song(self, request: HttpRequest, id_album):
         with connection.cursor() as cursor:
-            query = 'SELECT id, nama FROM label'
+            query = 'SELECT judul FROM album WHERE album.id = %s'
+            cursor.execute(query, [id_album])
+
+            album_name = cursor.fetchone()[0]
+
+            query = 'SELECT id, nama FROM artist JOIN akun ON artist.email_akun = akun.email'
             cursor.execute(query)
 
             entries = cursor.fetchall()
-            columns = [col[0] for col in cursor.description]
-            labels = [dict(zip(columns, row)) for row in entries]
+            columns = ['id', 'nama']
+            artists = [dict(zip(columns, row)) for row in entries]
 
-        context = {'labels': labels}
+            query = 'SELECT id, nama FROM songwriter JOIN akun ON songwriter.email_akun = akun.email'
+            cursor.execute(query)
 
-        return render(request, 'addAlbum.html', context=context)
+            entries = cursor.fetchall()
+            columns = ['id', 'nama']
+            songwriters = [dict(zip(columns, row)) for row in entries]
+
+            query = 'SELECT genre.id_konten, genre.genre FROM genre JOIN song ON genre.id_konten = song.id_konten'
+            cursor.execute(query)
+
+            entries = cursor.fetchall()
+            columns = ['id_konten', 'genre']
+            genres = [dict(zip(columns, row)) for row in entries]
+
+        context = {
+            'album_name': album_name,
+            'artists': artists,
+            'songwriters': songwriters,
+            'genres': genres
+        }
+
+        return render(request, 'addSongToAlbum.html', context=context)
     
-    def __get_album_list_label_page(self, request: HttpRequest):
-        return render(request, 'albumListLabel.html')
+    def __get_delete_album(self, request: HttpRequest, id_album):
+        pass
     
-    def __get_album_songs_label_page(self, request: HttpRequest, id_album):
-        return render(request, 'albumSongs.html')
-    
-    def get(self, request: HttpRequest, id_album = None):
-        req_full_path = request.get_full_path()
-
-        if id_album is None:
-            if req_full_path.endswith('/album/'):
-                return self.__get_album_list_page(request)
-            elif req_full_path.endswith('/add-album/'):
-                return self.__get_add_album_page(request)
-            elif req_full_path.endswith('/label/'):
-                return self.__get_album_list_label_page(request)
-        elif req_full_path.endswith('/add-song/'):
-            return self.__get_add_song_page(request, id_album)
-        elif req_full_path.endswith('/songs/'):
-            return self.__get_album_songs_page(request, id_album)
-        elif req_full_path.endswith('/label/'):
-            return self.__get_album_songs_label_page(request, id_album)
-
-    def post(self, request: HttpRequest):
-        req_body_dict = json.loads(request.body)
-
-        id_album = uuid.uuid4()
-        judul = req_body_dict['judul']
-        id_label = req_body_dict['id_label']
-        
-        with connection.cursor() as cursor:
-            query = 'INSERT INTO album (id, judul, id_label) VALUES (%s, %s, %s)'
-            cursor.execute(query, [id_album, judul, id_label])
-
-        return HttpResponse(status=201)
-    
-    def delete(self, request: HttpRequest, id_album):
-        req_full_path = request.get_full_path()
-
+    def __delete_album(self, request: HttpRequest, id_album):
         with connection.cursor() as cursor:
             query = 'DELETE FROM album WHERE id = %s'
             cursor.execute(query, [id_album])
 
         return HttpResponse(status=204)
+
+    def get(self, request: HttpRequest, id_album):
+        req_full_path = request.get_full_path()
+
+        if req_full_path.endswith('/songs/'):
+            return self.__get_songs(request, id_album)
+        elif req_full_path.endswith('/add-song/'):
+            return self.__get_add_song(request, id_album)
+        elif req_full_path.endswith('/delete/'):
+            return self.__get_delete_album(request, id_album)
+    
+    def delete(self, request: HttpRequest, id_album):
+        req_full_path = request.get_full_path()
+
+        if req_full_path.endswith('/delete/'):
+            return self.__delete_album(request, id_album)
